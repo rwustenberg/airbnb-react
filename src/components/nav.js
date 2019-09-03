035882eeaf3dfd093603f5a773362b97e3525e74
ruthw <rwustenberg@gmail.com>
2019-09-03 05:21:11 -0500

    components

---
+++ src/components/nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/grid.css'
+
+class Nav extends React.Component {
+	state= {
+
+	}
+		render () {
+			return(
+		<nav>
+			<Link to="/" className="logo" style={{backgroundImage: 'url(images/logo-airbnb.png)'}}></Link>
+			<div className="profile">
+				<Link to="profile.html" className="button">
+					<div className="avatar" style={{backgroundImage: 'url(https://randomuser.me/api/portraits/men/9.jpg)'}}></div>
+					<span>Tony</span>
+				</Link>
+			</div>
+		</nav>
+		)
+	}
+}
+
+export default Nav
---
+++ src/components/sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../styles/sidebar.css'
+
+
+class Sidebar extends React.Component {
+	render () {
+		return(
+		<div className="grid medium">
+			<div className="grid sidebar-left">
+				<div className="sidebar">
+					<ul>
+						<li className="">
+							<Link to="profile.html">Profile</Link>
+						</li>
+						<li className="">
+							<Link to="bookings.html">Bookings</Link>
+						</li>
+						<li className="">
+							<Link to="favorites.html">Favorites</Link>
+						</li>
+						<li className="active">
+							<Link to="host.html">Host</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		)
+	}
+}
+
+export default Sidebar;
